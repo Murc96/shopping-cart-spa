a726997227c4998e8b4e109b1809ac92fea90d13
Murc96 <133204473+Murc96@users.noreply.github.com>
2024-02-25 19:40:52 +0100

    some design changes

--- src/components/ShoppingCartPage.tsx
+++ src/components/ShoppingCartPage.tsx
@@ -4,20 +4,21 @@ export default function ShoppingCartPage() {
   const { cart, removeFromCart } = useShoppingCartContext(); // Verwenden Sie den Context-Hook
 
   return (
-    <div>
-      <h2>Shopping Cart</h2>
+    <div className="container-cart">
+      <h2 className='cart-headline'>Shopping Cart</h2>
       {cart.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
         <div>
           {cart.map(product => (
-            <div key={product.id}>
-              <p>{product.title}</p>
-              <p>{product.price}</p>
-              <button onClick={() => removeFromCart(product.id)}>Remove</button>
+            <div key={product.id} className='cart-product-container'>
+              <img className="product-img" src={product.image} alt={product.title} />
+              <p className='cart-product-title'>{product.title}</p>
+              <p className="cart-product-price">{product.price}</p>
+              <button onClick={() => removeFromCart(product.id)} className='remove-btn'>Remove</button>
             </div>
           ))}
-          <button>Checkout</button>
+          <button className='checkout-btn'>Checkout</button>
         </div>
       )}
     </div>
